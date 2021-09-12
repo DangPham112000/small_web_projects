39a77d36dca4c6230b12e63f5a3c9d0858b648ea
DangPham <dangpham112000@users.noreply.github.com>
2021-09-12 19:25:27 +0700

    Done github profiles app

--- 20_github_profiles_app/script.js
+++ 20_github_profiles_app/script.js
@@ -4,14 +4,23 @@ const formEl = document.querySelector('#form');
 const mainEl = document.querySelector('#main');
 const searchEl = document.querySelector('#search');
 
-getUser('florinpop17');
+getUser('dangpham112000');
 
 
-async function getUser(user) {
-    const resp = await fetch(GITHUB_API + user);
+async function getUser(username) {
+    const resp = await fetch(GITHUB_API + username);
     const respData = await resp.json();
 
     createUserCard(respData);
+
+    getRepos(username);
+}
+
+async function getRepos(username) {
+    const resp = await fetch(GITHUB_API + username + '/repos');
+    const respData = await resp.json();
+
+    addReposToCard(respData);
 }
 
 function createUserCard(userData) {
@@ -40,10 +49,29 @@ function createUserCard(userData) {
                     ${userData.public_repos}
                 </li>
             </ul>
+
+            <div class="repos" id="repos"></div>
         </div>
     `;
 
     mainEl.appendChild(cardEl);
+}
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos');
+
+    repos
+    .sort((a, b) => b.stargazers_count - a.stargazers_count)
+    .forEach(repo => {
+        const repoEl = document.createElement('a');
+        repoEl.classList.add('repo');
+
+        repoEl.href = repo.html_url;
+        repoEl.target = '_blank';
+        repoEl.innerText = repo.name;
+
+        reposEl.appendChild(repoEl);
+    });
 }
 
 formEl.addEventListener('submit', (e) => {
